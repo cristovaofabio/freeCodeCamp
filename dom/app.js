8407ec6382d3ba0b7e789358ff454d85fca6dcee
Fabio <cristovaofabio3@gmail.com>
2022-07-25 19:08:29 -0300

    feat: event listeners

--- dom/app.js
+++ dom/app.js
@@ -42,9 +42,38 @@ li.remove();
 const title = document.querySelector('#main-heading');
 console.log(title.innerHTML);
 */
-
+/*
 let ul = document.querySelector('ul');
 ul.childNodes[1].style.backgroundColor = 'green';
 console.log(ul.children);
 console.log(ul.firstElementChild);
 console.log(ul.lastElementChild);
+*/
+/*
+function alertBtn(){
+    return alert('I also love JavaScript');
+}
+
+const buttonTwo = document.querySelector('.btn-2');
+buttonTwo.addEventListener('click',alertBtn);
+
+const boxDiv = document.querySelector('.box-3');
+
+function changeBgColor(){
+    boxDiv.style.backgroundColor = 'blue';
+}
+boxDiv.addEventListener('mouseover',changeBgColor);
+*/
+
+const revealBtn = document.querySelector('.reveal-btn');
+const hiddenContent = document.querySelector('.hidden-content');
+
+function revealContent() {
+    if (hiddenContent.classList.contains('reveal-btn')) {
+        hiddenContent.classList.remove('reveal-btn');
+    } else {
+        hiddenContent.classList.add('reveal-btn');
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
