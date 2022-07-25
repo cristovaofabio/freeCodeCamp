a4f2eb381db083ec85b68cdd1302245798f28651
Fabio <cristovaofabio3@gmail.com>
2022-07-25 09:55:00 -0300

    feat: selecting and styling elements

---
+++ dom/app.js
@@ -0,0 +1,31 @@
+/*
+const title = document.getElementById('main-heading');
+console.log(title);
+
+const itemElement = document.getElementsByClassName('list-items');
+console.log(itemElement);
+
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
+
+const container = document.querySelector('div');
+console.log(container);
+
+const containers = document.querySelectorAll('div');
+console.log(containers);
+*/
+
+/*
+const title =  document.querySelector('#main-heading');
+title.style.color = 'red';
+console.log(title);
+
+const listItems = document.querySelectorAll('.list-items');
+console.log(listItems);
+
+listItems.forEach((item)=>{
+    item.style.fontSize = '2rem';
+})
+
+
+*/
